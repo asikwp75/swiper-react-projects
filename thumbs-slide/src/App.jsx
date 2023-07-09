31113374cc10f629c2de57ce0e517f76d9217ae1
asikurrahman75 <asikur@bdthemes.com>
2023-07-09 22:17:22 +0600

    style done

--- thumbs-slide/src/App.jsx
+++ thumbs-slide/src/App.jsx
@@ -1,182 +1,199 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { SwiperNavButton } from './components/SwiperNavigation';
+import './App.css';
 
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/effect-fade';
 import 'swiper/css/free-mode';
-import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 
-import './app.css';
+
 
 // import required modules
-import { Parallax, FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { EffectFade, Parallax, FreeMode, Navigation, Thumbs } from 'swiper/modules';
 
 export default function App() {
-
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
     <>
-      <div className='container'>
-        <Swiper
-          style={{
-            '--swiper-navigation-color': '#fff',
-            '--swiper-pagination-color': '#fff',
-          }}
-          loop={true}
-          spaceBetween={10}
-          navigation={true}
-          speed={600}
-          parallax={true}
-          thumbs={{ swiper: thumbsSwiper }}
-          modules={[Parallax, FreeMode, Navigation, Thumbs]}
-          className="mySwiper2"
-        >
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-            <div className='content-wrap'>
-              <div className="title" data-swiper-parallax="-800">
-                Slide 1
-              </div>
-              <div className="subtitle" data-swiper-parallax="-800">
-                Subtitle
-              </div>
-              <div className="text" data-swiper-parallax="-900">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                  dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                  laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-                  Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-                  Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                  ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                  tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
+      <div className='zb-container'>
+        <div className="zb-post-slider-wrap">
+          <Swiper
+            loop={true}
+            spaceBetween={0}
+            speed={600}
+            parallax={true}
+            effect={'fade'}
+            thumbs={{ swiper: thumbsSwiper }}
+            modules={[EffectFade, Parallax, FreeMode, Thumbs]}
+            className="zb-post-main-slider-wrap">
 
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-            <div className='content-wrap'>
-              <div className="title" data-swiper-parallax="-300">
-                Slide 1
-              </div>
-              <div className="subtitle" data-swiper-parallax="-200">
-                Subtitle
-              </div>
-              <div className="text" data-swiper-parallax="-100">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                  dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                  laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-                  Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-                  Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                  ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                  tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-            <div className='content-wrap'>
-              <div className="title" data-swiper-parallax="-300">
-                Slide 1
-              </div>
-              <div className="subtitle" data-swiper-parallax="-200">
-                Subtitle
-              </div>
-              <div className="text" data-swiper-parallax="-100">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                  dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                  laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-                  Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-                  Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                  ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                  tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-            <div className='content-wrap'>
-              <div className="title" data-swiper-parallax="-300">
-                Slide 1
-              </div>
-              <div className="subtitle" data-swiper-parallax="-200">
-                Subtitle
-              </div>
-              <div className="text" data-swiper-parallax="-100">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                  dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                  laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-                  Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-                  Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                  ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                  tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-            <div className='content-wrap'>
-              <div className="title" data-swiper-parallax="-300">
-                Slide 1
-              </div>
-              <div className="subtitle" data-swiper-parallax="-200">
-                Subtitle
-              </div>
-              <div className="text" data-swiper-parallax="-100">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-                  dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-                  laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-                  Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-                  Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-                  ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-                  tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
+            <SwiperSlide>
+                <div className="zb-post-image-wrap">
+                  <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                </div>
+                <div className='zb-post-content-wrap'>
+                     <div className="zb-post-category" data-swiper-parallax-y="-100">
+                         <a href="#">Business</a>
+                     </div>
+                     <div className="zb-post-title" data-swiper-parallax-y="-80">
+                        <a href="#">The Meticulous Writer’s 12-Point Blogging Checklist</a>
+                     </div>
+                </div>
+            </SwiperSlide>
 
-        </Swiper>
+            <SwiperSlide>
+              <div className="zb-post-image-wrap">
+                <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+              </div>
+                <div className='zb-post-content-wrap'>
+                     <div className="zb-post-category" data-swiper-parallax-y="-100">
+                         <a href="#">Business</a>
+                     </div>
+                     <div className="zb-post-title" data-swiper-parallax-y="-80">
+                        <a href="#">The Meticulous Writer’s 12-Point Blogging Checklist</a>
+                     </div>
+                </div>
+            </SwiperSlide>
 
+            <SwiperSlide>
+              <div className="zb-post-image-wrap">
+                <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+              </div>
+                <div className='zb-post-content-wrap'>
+                     <div className="zb-post-category" data-swiper-parallax-y="-100">
+                         <a href="#">Business</a>
+                     </div>
+                     <div className="zb-post-title" data-swiper-parallax-y="-80">
+                        <a href="#">The Meticulous Writer’s 12-Point Blogging Checklist</a>
+                     </div>
+                </div>
+            </SwiperSlide>
 
-        <Swiper
-          onSwiper={setThumbsSwiper}
-          loop={true}
-          speed={600}
-          spaceBetween={10}
-          slidesPerView={4}
-          freeMode={true}
-          watchSlidesProgress={true}
-          modules={[FreeMode, Navigation, Thumbs]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <SwiperSlide>
+              <div className="zb-post-image-wrap">
+                <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+              </div>
+                <div className='zb-post-content-wrap'>
+                     <div className="zb-post-category" data-swiper-parallax-y="-100">
+                         <a href="#">Business</a>
+                     </div>
+                     <div className="zb-post-title" data-swiper-parallax-y="-80">
+                        <a href="#">The Meticulous Writer’s 12-Point Blogging Checklist</a>
+                     </div>
+                </div>
+            </SwiperSlide>
 
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-          </SwiperSlide>
-        </Swiper>
+            <SwiperSlide>
+              <div className="zb-post-image-wrap">
+                <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+              </div>
+                <div className='zb-post-content-wrap'>
+                     <div className="zb-post-category" data-swiper-parallax-y="-100">
+                         <a href="#">Business</a>
+                     </div>
+                     <div className="zb-post-title" data-swiper-parallax-y="-80">
+                        <a href="#">The Meticulous Writer’s 12-Point Blogging Checklist</a>
+                     </div>
+                </div>
+            </SwiperSlide>
 
+            <SwiperNavButton />
+          </Swiper>
+
+          <Swiper
+
+            onSwiper={setThumbsSwiper}
+            loop={true}
+            speed={600}
+            spaceBetween={10}
+            slidesPerView={4}
+            direction={'vertical'}
+            freeMode={true}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+            className="zb-thumbs-slider-wrap">
+
+            <SwiperSlide className='zb-post-slide-item'>
+              <div className="zb-post-desc">
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in</p>
+              </div>
+              <div className="zb-post-meta">
+                <div className="zb-post-author">
+                  <span>by</span>
+                  <a href="#">muhammad asik</a>
+                </div>
+                <div className="zb-post-date">
+                  <span>july 9, 2023</span>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide className='zb-post-slide-item'>
+              <div className="zb-post-desc">
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in</p>
+              </div>
+              <div className="zb-post-meta">
+                <div className="zb-post-author">
+                  <span>by</span>
+                  <a href="#">muhammad asik</a>
+                </div>
+                <div className="zb-post-date">
+                  <span>july 9, 2023</span>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide className='zb-post-slide-item'>
+              <div className="zb-post-desc">
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in</p>
+              </div>
+              <div className="zb-post-meta">
+                <div className="zb-post-author">
+                  <span>by</span>
+                  <a href="#">muhammad asik</a>
+                </div>
+                <div className="zb-post-date">
+                  <span>july 9, 2023</span>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide className='zb-post-slide-item'>
+              <div className="zb-post-desc">
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in</p>
+              </div>
+              <div className="zb-post-meta">
+                <div className="zb-post-author">
+                  <span>by</span>
+                  <a href="#">muhammad asik</a>
+                </div>
+                <div className="zb-post-date">
+                  <span>july 9, 2023</span>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide className='zb-post-slide-item'>
+              <div className="zb-post-desc">
+                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in</p>
+              </div>
+              <div className="zb-post-meta">
+                <div className="zb-post-author">
+                  <span>by</span>
+                  <a href="#">muhammad asik</a>
+                </div>
+                <div className="zb-post-date">
+                  <span>july 9, 2023</span>
+                </div>
+              </div>
+            </SwiperSlide>
+
+          </Swiper>
+        </div>
       </div>
 
     </>
